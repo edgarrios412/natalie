d21f76b73136f47e45f21338eddcdf1c14d6e1ff
unknown <edgarrios412@gmail.com>
2023-12-06 11:52:39 -0500

    push

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import style from './Footer.module.css'
 import {RiFacebookCircleLine} from "react-icons/ri"
 import {FiPhoneCall, FiInstagram} from "react-icons/fi"
 import {BsInstagram} from "react-icons/bs"
+import {LuShieldQuestion} from "react-icons/lu"
 import {MdOutlineLocationOn} from "react-icons/md"
 import {FaWhatsapp} from "react-icons/fa"
 
@@ -14,7 +15,7 @@ const Footer = () => {
             <a target='_blank' className={style.link} href="https://www.instagram.com/nataliearizaodontestetica/ "><FiInstagram className={style.oneIcono}/></a>
             <a target='_blank' className={style.link} href="https://goo.gl/maps/5AZAVcY3uA3oHwS38 "><MdOutlineLocationOn className={style.oneIcono}/></a>
             <a target='_blank' className={style.link} href="https://wa.link/sdh5gu"><FaWhatsapp className={style.oneIcono}/></a>
-            <a target='_blank' className={style.link} href="/politicas"><FaWhatsapp className={style.oneIcono}/></a>
+            <a target='_blank' className={style.link} href="/terminos"><LuShieldQuestion className={style.oneIcono}/></a>
         </div>
         <p className={style.creadorDavid}><a className={style.link} target="_blank" href='https://www.codeandcoffee.tech'>Powered by CodeandCoffee</a></p>
     </footer>
